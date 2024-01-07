4eb3fa3b05edd81ad45a17936bf69f6410ac2819
brobo1 <armanahz95@gmail.com>
2024-01-07 18:32:35 +0100

    Remove commented out code in HomeController
    The commented out part of the Index action in the HomeController was removed. This cleanup improves readability and reduces unnecessary clutter in the codebase.

--- NationalizeNameApi/Controllers/HomeController.cs
+++ NationalizeNameApi/Controllers/HomeController.cs
@@ -21,16 +21,6 @@ public class HomeController : Controller {
 		return View(new IndexViewModel());
 	}
 
-/*
-	[HttpPost("/")]
-	public async Task<IActionResult> Index(IndexViewModel model) {
-		var country = await _natApi.GetCountryAsync(model.Name);
-		var code    = await _countryFlagApi.GetCountryFlagAsync(country.Country[0].CountryId);
-		model.CountryResponse     = country;
-		model.CountryFlagResponse = code;
-		return View(model);
-	}
-	*/
 	[HttpPost("/")]
 	public async Task<IActionResult> Index(IndexViewModel model) {
 		var country = await _natApi.GetCountryAsync(model.Name);
